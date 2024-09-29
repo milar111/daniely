78983dd1b5e0142b28a94c45de8b14228561834d
milar111 <114105495+milar111@users.noreply.github.com>
2024-09-29 13:30:55 +0300

    created ProjectCard

--- portfoliov2/components/pages/Projects.tsx
+++ portfoliov2/components/pages/Projects.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { projects, icons } from '@/data';
-import { useRouter } from 'next/navigation';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import { projects } from '@/data';
+import ProjectCard from '../ui/ProjectCard';
 
 const Projects = () => {
   const [windowWidth, setWindowWidth] = useState<number>(0);
-  const router = useRouter();
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth);
@@ -14,10 +11,6 @@ const Projects = () => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-
-  const handleProjectClick = (id: string) => {
-    router.push(`/${id}`);
-  };
 
   return (
     <div className='py-20 relative min-h-screen flex flex-col justify-center items-center space-y-10 w-full'>
@@ -32,137 +25,9 @@ const Projects = () => {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-5 xsm:p-1">
-        {projects.map((project, index) => {
-          const [currentImageIndex, setCurrentImageIndex] = useState(0);
-          const { ref, inView } = useInView({
-            triggerOnce: true,
-            threshold: 0.1,
-          });
-
-          const isLeftCard = index % 2 === 0;
-          const initialX = isLeftCard ? '-15%' : '15%';
-
-          const nextImage = (e: React.MouseEvent) => {
-            e.stopPropagation(); 
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % project.image.length);
-          };
-
-          const prevImage = (e: React.MouseEvent) => {
-            e.stopPropagation(); 
-            setCurrentImageIndex((prevIndex) => (prevIndex - 1 + project.image.length) % project.image.length);
-          };
-
-          // Determine animation based on screen size
-          const cardAnimation = windowWidth > 1024 ? {
-            initial: { opacity: 0, x: initialX },
-            animate: { opacity: inView ? [0, 0.5, 1] : 0, x: inView ? 0 : initialX },
-            transition: { x: { duration: 0.5, ease: 'easeOut' }, opacity: { duration: 0.5, ease: 'easeOut', delay: 0.25 } }
-          } : {
-            initial: { opacity: 0 },
-            animate: { opacity: inView ? 1 : 0 },
-            transition: { opacity: { duration: 0.5, ease: 'easeOut' } }
-          };
-
-          return (
-            <motion.div
-              ref={ref}
-              initial={cardAnimation.initial}
-              animate={cardAnimation.animate}
-              transition={cardAnimation.transition}
-              key={index}
-              onClick={() => handleProjectClick(project.id)}
-              className="bg-lightGray border-[2px] shadow-bottom-left border-borderProject rounded-2xl p-5 flex flex-col relative 
-              filter grayscale hover:grayscale-0 transition duration-500 w-full max-w-[400px] h-[470px] cursor-custom-card"
-            >
-              <div
-                className='bg-grayISH w-full h-[224px] rounded-2xl border-black flex justify-center items-center mb-4 relative overflow-hidden'
-                onClick={(e) => handleProjectClick(project.id)}
-              >
-                <div className='relative w-full h-full'>
-                  {project.image.map((imgSrc, imgIndex) => (
-                    <img
-                      key={imgIndex}
-                      src={imgSrc}
-                      alt={project.title}
-                      className={`absolute h-full w-full object-cover rounded-2xl transition-opacity duration-500 
-                        ${imgIndex === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
-                    />
-                  ))}
-                </div>
-
-                <div className="absolute bottom-4 right-4 flex space-x-1">
-                  <div
-                    className='border border-border bg-darkGray rounded-full w-7 h-7 flex justify-center items-center xxsm:w-6 xxsm:h-6'
-                    onClick={prevImage} 
-                  >
-                    <img src="./icons/left.svg" alt="Left Arrow" className="w-6 h-6 flex items-center justify-center xxsm:w-5 xxsm:h-5" />
-                  </div>
-                  <div
-                    className='border border-border bg-darkGray rounded-full w-7 h-7 flex justify-center items-center xxsm:w-6 xxsm:h-6'
-                    onClick={nextImage} 
-                  >
-                    <img src="./icons/right.svg" alt="Right Arrow" className="w-6 h-6 flex items-center justify-center xxsm:w-5 xxsm:h-5" />
-                  </div>
-                </div>
-              </div>
-
-              <h1 className='text-2xl font-bold font-inconsolata xxsm:text-xl'>
-                <span>{project.title}</span>
-              </h1>
-
-              <h1 className='text-xl font-inconsolata text-fontLG2 font-normal xxsm:text-base'>
-                <span>{project.description}</span>
-              </h1>
-
-              <div className='absolute bottom-16 xxsm:bottom-[3.5rem] left-3'>
-                {project.technologies.map((techId, techIndex) => {
-                  const techIcon = icons.find(icon => icon.id === techId);
-                  const translationX = windowWidth > 380 ? 35 * techIndex + 5 : 26 * techIndex + 5;
-
-                  return (
-                    <div
-                      key={techIndex}
-                      style={{
-                        transform: `translateX(${translationX}px)`,
-                        zIndex: `${project.technologies.length - techIndex}`,
-                        position: 'absolute',
-                      }}
-                      className='border border-border bg-darkGray rounded-full w-11 h-11 flex justify-center items-center filter grayscale 
-                      xxsm:w-9 xxsm:h-9'
-                    >
-                      {techIcon && (
-                        <img src={techIcon.icon} alt={techIcon.name} className="w-6 h-6 xxsm:w-5 xxsm:h-5" />
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-
-              <div className="absolute bottom-6 right-6 flex space-x-1">
-                {project.GitHubLink && (
-                  <a 
-                    href={project.GitHubLink} 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    onClick={(e) => e.stopPropagation()} 
-                  >
-                    <img src="./icons/github.svg" alt="GitHub" className="w-8 h-8 xxsm:w-6 xxsm:h-6" />
-                  </a>
-                )}
-                {project.YouTubeLink && (
-                  <a 
-                    href={project.YouTubeLink} 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    onClick={(e) => e.stopPropagation()} 
-                  >
-                    <img src="./icons/youtube.svg" alt="YouTube" className="w-8 h-8 xxsm:w-6 xxsm:h-6" />
-                  </a>
-                )}
-              </div>
-            </motion.div>
-          );
-        })}
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} index={index} windowWidth={windowWidth} />
+        ))}
       </div>
     </div>
   );
---
+++ portfoliov2/components/ui/ProjectCard.tsx
@@ -0,0 +1,163 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { icons } from '@/data';
+import { useRouter } from 'next/navigation';
+
+interface ProjectCardProps {
+  project: {
+    id: string;
+    title: string;
+    description: string;
+    image: string[];
+    technologies: string[];
+    GitHubLink?: string;
+    YouTubeLink?: string;
+  };
+  index: number;
+  windowWidth: number;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ project, index, windowWidth }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const router = useRouter();
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const isLeftCard = index % 2 === 0;
+  const initialX = isLeftCard ? '-15%' : '15%';
+
+  const nextImage = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % project.image.length);
+  };
+
+  const prevImage = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + project.image.length) % project.image.length);
+  };
+
+  const cardAnimation = windowWidth > 1024 ? {
+    initial: { opacity: 0, x: initialX },
+    animate: { opacity: inView ? [0, 0.5, 1] : 0, x: inView ? 0 : initialX },
+    transition: { x: { duration: 0.5, ease: 'easeOut' }, opacity: { duration: 0.5, ease: 'easeOut', delay: 0.25 } }
+  } : {
+    initial: { opacity: 0 },
+    animate: { opacity: inView ? 1 : 0 },
+    transition: { opacity: { duration: 0.5, ease: 'easeOut' } }
+  };
+
+  const handleProjectClick = () => {
+    router.push(`/${project.id}`);
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={cardAnimation.initial}
+      animate={cardAnimation.animate}
+      transition={cardAnimation.transition}
+      onClick={handleProjectClick}
+      className="bg-lightGray border-[2px] shadow-bottom-left border-borderProject rounded-2xl p-5 flex flex-col relative 
+      filter grayscale hover:grayscale-0 transition duration-500 w-full max-w-[400px] h-[470px] cursor-custom-card"
+    >
+      <div
+        className='bg-grayISH w-full h-[224px] rounded-2xl border-black flex justify-center items-center mb-4 relative overflow-hidden'
+        onClick={handleProjectClick}
+      >
+        <div className='relative w-full h-full'>
+          {project.image.map((imgSrc, imgIndex) => (
+            <img
+              key={imgIndex}
+              src={imgSrc}
+              alt={project.title}
+              className={`absolute h-full w-full object-cover rounded-2xl transition-opacity duration-500 
+                ${imgIndex === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
+            />
+          ))}
+        </div>
+
+        <div className="absolute bottom-4 right-4 flex space-x-1">
+          <div
+            className='border border-border bg-darkGray rounded-full w-7 h-7 flex justify-center items-center xxsm:w-6 xxsm:h-6'
+            onClick={prevImage} 
+          >
+            <img src="./icons/left.svg" alt="Left Arrow" className="w-6 h-6 flex items-center justify-center xxsm:w-5 xxsm:h-5" />
+          </div>
+          <div
+            className='border border-border bg-darkGray rounded-full w-7 h-7 flex justify-center items-center xxsm:w-6 xxsm:h-6'
+            onClick={nextImage} 
+          >
+            <img src="./icons/right.svg" alt="Right Arrow" className="w-6 h-6 flex items-center justify-center xxsm:w-5 xxsm:h-5" />
+          </div>
+        </div>
+      </div>
+
+      {/* Title and Description that won't trigger navigation on click */}
+      <h1
+        className='text-2xl font-bold font-inconsolata xxsm:text-xl'
+        onClick={(e) => e.stopPropagation()} // Prevents click from opening the project page
+      >
+        <span>{project.title}</span>
+      </h1>
+
+      <h1
+        className='text-xl font-inconsolata text-fontLG2 font-normal xxsm:text-base'
+        onClick={(e) => e.stopPropagation()} // Prevents click from opening the project page
+      >
+        <span>{project.description}</span>
+      </h1>
+
+      <div className='absolute bottom-16 xxsm:bottom-[3.5rem] left-3'>
+        {project.technologies.map((techId, techIndex) => {
+          const techIcon = icons.find(icon => icon.id === techId);
+          const translationX = windowWidth > 380 ? 35 * techIndex + 5 : 26 * techIndex + 5;
+
+          return (
+            <div
+              key={techIndex}
+              style={{
+                transform: `translateX(${translationX}px)`,
+                zIndex: `${project.technologies.length - techIndex}`,
+                position: 'absolute',
+              }}
+              className='border border-border bg-darkGray rounded-full w-11 h-11 flex justify-center items-center filter grayscale 
+              xxsm:w-9 xxsm:h-9'
+            >
+              {techIcon && (
+                <img src={techIcon.icon} alt={techIcon.name} className="w-6 h-6 xxsm:w-5 xxsm:h-5" />
+              )}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="absolute bottom-6 right-6 flex space-x-1">
+        {project.GitHubLink && (
+          <a 
+            href={project.GitHubLink} 
+            target="_blank" 
+            rel="noopener noreferrer"
+            onClick={(e) => e.stopPropagation()} // Prevents opening the project page when clicking on GitHub link
+          >
+            <img src="./icons/github.svg" alt="GitHub" className="w-8 h-8 xxsm:w-6 xxsm:h-6" />
+          </a>
+        )}
+        {project.YouTubeLink && (
+          <a 
+            href={project.YouTubeLink} 
+            target="_blank" 
+            rel="noopener noreferrer"
+            onClick={(e) => e.stopPropagation()} // Prevents opening the project page when clicking on YouTube link
+          >
+            <img src="./icons/youtube.svg" alt="YouTube" className="w-8 h-8 xxsm:w-6 xxsm:h-6" />
+          </a>
+        )}
+      </div>
+    </motion.div>
+  );
+};
+
+export default ProjectCard;
