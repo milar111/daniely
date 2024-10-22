de6b0ebd97adb00568aac476bf90a5b7ac4af889
milar111 <114105495+milar111@users.noreply.github.com>
2024-10-22 21:56:55 +0300

    small changes made. Tryin to fix cursor not showing.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true, // Disable Image Optimization for static export
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: "export",
+  images: {
+    unoptimized: true, // Disable Image Optimization for static export
+  },
+  basePath: '/daniely', // Add repository name for GitHub Pages
+  assetPrefix: '/daniely/', // Ensure asset paths are prefixed with repository name
+};
+
+export default nextConfig;
