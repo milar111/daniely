1d12bb5b727877e3af52ccf75a00eb0fcffab39c
milar111 <114105495+milar111@users.noreply.github.com>
2024-10-26 00:24:29 +0300

    Revert "small changes made. Tryin to fix cursor not showing."
    This reverts commit de6b0ebd97adb00568aac476bf90a5b7ac4af889.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // Disable Image Optimization for static export
-  },
-  basePath: '/daniely', // Add repository name for GitHub Pages
-  assetPrefix: '/daniely/', // Ensure asset paths are prefixed with repository name
-};
-
-export default nextConfig;
+    output: "export",
+    images: {
+      unoptimized: true, // Disable Image Optimization for static export
+    },
+  };
+  
+  export default nextConfig;
+  
