bcb806fbb199c1b6e51d1547351a39b4ad5db2c4
milar111 <114105495+milar111@users.noreply.github.com>
2024-10-26 15:10:51 +0300

    TestCursorGitHubPages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,20 @@
-"use client";
+"use client"; // Mark the component as a Client Component
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-//declare the metadata directly in the page component instead.
 const metadata = {
   title: "Daniel Yordanov",
   description: "Portfolio website.",
 };
-
-function updateCursorPaths() {
-  const isLocalhost = window.location.hostname === 'localhost';
-  const basePath = isLocalhost ? '' : '/daniely';
-
-  const updateCursorClass = (className: string, cursorPath: string) => {
-    const elements = document.querySelectorAll<HTMLElement>(`.` + className);
-    elements.forEach((el) => {
-      el.style.cursor = `url('${basePath}${cursorPath}'), ${className.includes('link') ? 'pointer' : 'auto'}`;
-    });
-  };
-
-  updateCursorClass('cursor-custom-card', '/cursor/default.svg');
-  updateCursorClass('cursor-custom-default', '/cursor/default.svg');
-  updateCursorClass('cursor-custom-text', '/cursor/text.svg');
-  updateCursorClass('cursor-custom-link', '/cursor/link.svg');
-}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    updateCursorPaths();
-  }, []);
-
   return (
     <html lang="en">
       <head>
