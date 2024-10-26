1d014a42e4ce8b8c681f9e6aacd1cc67e17549ce
milar111 <114105495+milar111@users.noreply.github.com>
2024-10-26 11:00:36 +0300

    next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/daniely' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/daniely/' : '',
-};
 
-export default nextConfig;
-=======
     output: "export",
     images: {
       unoptimized: true, // Disable Image Optimization for static export
@@ -18,5 +8,3 @@ export default nextConfig;
   };
   
   export default nextConfig;
-  
->>>>>>> parent of de6b0eb (small changes made. Tryin to fix cursor not showing.)
