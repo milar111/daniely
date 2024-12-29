4d7155c9f381fe57ee25bade854329637619c725
milar111 <114105495+milar111@users.noreply.github.com>
2024-12-29 21:59:09 +0200

    smooth scroll added

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,19 @@ import { useEffect } from 'react';
 import Hero from "@/components/pages/Hero";
 import Projects from "@/components/pages/Projects";
 import './globals.css';
+import Lenis from 'lenis';
 
 export default function Home() {
+
+  useEffect(() =>{
+    const lenis = new Lenis();
+    function raf(time: any){
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
     <main className="custom-cursor flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
